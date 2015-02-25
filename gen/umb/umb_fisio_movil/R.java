2c172a4ba3d8ffb93e9ccadece9d22f9f809baa9
Daniela <dapina.pianda@inger.co>
2015-02-25 16:57:43 -0500

    blu

--- gen/umb/umb_fisio_movil/R.java
+++ gen/umb/umb_fisio_movil/R.java
@@ -1648,10 +1648,11 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f09003f;
+        public static final int action_settings=0x7f090044;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
+        public static final int button_scan=0x7f090043;
         public static final int checkbox=0x7f090029;
         public static final int collapseActionView=0x7f090015;
         public static final int decor_content_parent=0x7f09002f;
@@ -1672,8 +1673,10 @@ containing a value of this type.
         public static final int list_item=0x7f090025;
         public static final int middle=0x7f09001d;
         public static final int never=0x7f090017;
+        public static final int new_devices=0x7f090042;
         public static final int none=0x7f09000f;
         public static final int normal=0x7f09000b;
+        public static final int paired_devices=0x7f090040;
         public static final int progress_circular=0x7f090007;
         public static final int progress_horizontal=0x7f090008;
         public static final int radio=0x7f09002b;
@@ -1695,6 +1698,8 @@ containing a value of this type.
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
         public static final int title=0x7f090027;
+        public static final int title_new_devices=0x7f090041;
+        public static final int title_paired_devices=0x7f09003f;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
         public static final int withText=0x7f090018;
@@ -1749,11 +1754,14 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030014;
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_device_list=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int device_name=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int device_list=0x7f0c0000;
+        public static final int main=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1815,6 +1823,8 @@ containing a value of this type.
         public static final int action_settings=0x7f0a0012;
         public static final int app_name=0x7f0a0010;
         public static final int hello_world=0x7f0a0011;
+        public static final int none_paired=0x7f0a0014;
+        public static final int title_activity_device_list=0x7f0a0013;
     }
     public static final class style {
         /** 
